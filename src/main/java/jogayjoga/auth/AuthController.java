45d734e7a1e7abdc952c501991695f920c6a1cad
eriksoaress <erikbtu2020@gmail.com>
2024-04-04 18:02:39 -0300

    name feignclient

--- src/main/java/jogayjoga/auth/AuthController.java
+++ src/main/java/jogayjoga/auth/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("ingressify-auth")
+@FeignClient("jogayjoga-auth")
 public interface AuthController { 
 
         @PostMapping("/auth/register")
